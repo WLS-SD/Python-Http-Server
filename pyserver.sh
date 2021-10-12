bfd9025524e0d5756966a54c9611c2c24c838ec7
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-10-12 11:41:45 +0545

    Add files via upload

---
+++ pyserver.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo " Please Choose Y/N  (Y) is for Yes (Default port) & (N) is for No (Custom Port)"
+
+
+
+echo -n "Do you Want to use Default Port number (Y/N) :~"
+read default_or_custom_port
+
+
+if [ $default_or_custom_port = y ]
+then
+echo "Http Server will run on Port 8000 (Default Port)"
+python -m http.server 8000
+
+else
+
+if [ $default_or_custom_port = n ]
+then
+echo -n  "Enter Your Custom Port Number :~ "
+read user_port
+echo "Http Server is Started on Port $user_port"
+
+python -m http.server $user_port
+
+
+
+#echo "You Can Only Choose Y/N , Y is for Yes (Default port) & N is for No (Custom Port)"
+
+fi
+
+
+fi
+
+
