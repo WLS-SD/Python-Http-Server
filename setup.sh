97c30b80132d8c9f271535d1ecf96fac4ca343f0
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-10-14 08:21:09 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,12 +17,67 @@ printf "${GREEN}"
 
 apt install python -y
 
+
 sleep 3
+
 Light_Magenta="\e[95m"
 printf "${Light_Magenta}"
 
 echo "Required Dependencies installed"
 chmod +x *
+sleep 3
+
+
+
+echo -e '\e[96m
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ >     X  \...X
+ X# \.X        @#%,.@    MariaDb    @#%,.@.          X# \.X
+                @#%,.@              @#%,.@.
+                  @#%,.@          @#%,.@
+                     @#%,.@      @#%,.@
+                       @#%.,@  @#%,.@
+                       MySql/MariaDb
+'
+
+
+
+
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+sleep 0.2
+
+figlet -f standard "TOOL_by"
+
+sleep 0.2
+
+figlet -f standard "WLS"
+
+sleep 0.2
+
+echo "This Tool is Developed by https://github.com/WLS-SD under GNU GENERAL PUBLIC LICENSE Version 3."
+
+
+
+
+
+
+
 
 LightCyan="\e[96m"
 
