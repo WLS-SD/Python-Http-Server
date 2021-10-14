8c6d567917bbb4cda18212a3a825e76ee8301eb2
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-10-14 08:17:44 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
 
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+echo "installing Essential Dependencies"
+
+sleep 4
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
 apt install python -y
+
+sleep 3
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo "Required Dependencies installed"
+chmod +x *
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+
+echo "To Start Python-Http-Server execute './pyserver.sh'"
+
