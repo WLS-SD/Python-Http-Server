bfc98b8b770ea555cb8d8b4ab2b5fe82fa8a878f
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-10-14 08:24:03 +0545

    Update pyserver.sh

--- pyserver.sh
+++ pyserver.sh
@@ -1,6 +1,58 @@
 #!/bin/bash
 
 
+YELLOW="\e[93m"
+
+
+printf "${YELLOW}"
+
+
+
+echo "         ──▐─▌──▐─▌── "
+echo "         ─▐▌─▐▌▐▌─▐▌─ "
+echo "         ─█▄▀▄██▄▀▄█─ "
+echo "         ──▄──██▌─▄── "
+echo "         ▄▀─█▀██▀█─▀▄ "
+echo "         ▐▌▐▌─▐▌─▐▌▐▌ "
+echo "         ─▐─█────█─▌─ "
+echo "         ────▌──▐──── "
+echo "                      "
+
+
+sleep 0.2
+
+Green="\e[92m"
+
+
+printf "${Green}"
+
+
+
+echo -e  "   ▂▃▄▅▆▇▓▒░Coded_By_WLS░▒▓▇▆▅▄▃▂"
+echo "      -------------------------"
+
+sleep 0.2
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+
+
+
+
+echo "This Tool is Made by 'https://github.com/WLS-SD' Under GNU GENERAL PUBLIC LICENSE Version 3."
+ 
+
+
+sleep 2
+
+
+
+
+
+
 GREEN="\e[92m"
 
 
