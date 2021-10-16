e20a2b2a2ae5c08b2ce2a4dda9fe9cbe63a9b908
WLS <77196153+WLS-SD@users.noreply.github.com>
2021-10-16 03:20:46 +0000

    Add files via upload

---
+++ pyserver.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+
+YELLOW="\e[93m"
+
+
+printf "${YELLOW}"
+
+
+
+echo "         ──▐─▌──▐─▌── "
+echo "         ─▐▌─▐▌▐▌─▐▌─ "
+echo "         ─█▄▀▄██▄▀▄█─ "
+echo "         ──▄──██▌─▄── "
+echo "         ▄▀─█▀██▀█─▀▄ "
+echo "         ▐▌▐▌─▐▌─▐▌▐▌ "
+echo "         ─▐─█────█─▌─ "
+echo "         ────▌──▐──── "
+echo "                      "
+
+
+sleep 0.2
+
+Green="\e[92m"
+
+
+printf "${Green}"
+
+
+
+echo -e  "   ▂▃▄▅▆▇▓▒░Coded_By_WLS░▒▓▇▆▅▄▃▂"
+echo "      -------------------------"
+
+sleep 0.2
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+
+
+
+
+echo "This Tool is Made by 'https://github.com/WLS-SD' Under GNU GENERAL PUBLIC LICENSE Version 3."
+ 
+
+
+sleep 2
+
+
+
+
+
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+echo " Please Choose Y/N  (Y) is for Yes (Default port) & (N) is for No (Custom Port)"
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+echo -n "Do you Want to use Default Port number (Y/N) :~"
+read default_or_custom_port
+
+
+if [ $default_or_custom_port = y ]
+then
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo "Http Server will run on Port 8000 (Default Port)"
+python3 -m http.server 8000
+
+
+elif [ $default_or_custom_port = Y ]
+then
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo "Http Server will run on Port 8000 (Default Port)"
+python3 -m http.server 8000
+
+
+
+
+
+elif [ $default_or_custom_port = n ]
+then
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo -n  "Enter Your Custom Port Number :~ "
+read user_port
+echo "Http Server is Started on Port $user_port"
+
+python3 -m http.server $user_port
+
+
+
+elif [ $default_or_custom_port = N ]
+then
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo -n  "Enter Your Custom Port Number :~ "
+read user_port
+echo "Http Server is Started on Port $user_port"
+
+python3 -m http.server $user_port
+
+
+
+else
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+echo "You Can Only Choose Y/N , Y is for Yes (Default port) & N is for No (Custom Port)"
+
+fi
+
---
+++ setup-linux-system.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+GREEN="\e[92m"
+
+a=`pwd`
+
+printf "${GREEN}"
+cd
+name="pyserver"
+echo "Creating alias '${name}'..."
+echo -e "alias ${name}=\"$a/${name}.sh\"" >> .bashrc
+                                    
+cd $a
+
+
+
+printf "${LightCyan}"
+
+echo "installing Essential Dependencies"
+
+sleep 4
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+sudo apt install python3 -y
+
+sleep 2
+
+sudo apt install figlet -y
+
+
+sleep 3
+
+Light_Magenta="\e[95m"
+printf "${Light_Magenta}"
+
+echo "Required Dependencies installed"
+chmod +x *
+sleep 3
+
+
+
+echo -e '\e[96m
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ >     X  \...X
+ X# \.X        @#%,.@    MariaDb    @#%,.@.          X# \.X
+                @#%,.@              @#%,.@.
+                  @#%,.@          @#%,.@
+                     @#%,.@      @#%,.@
+                       @#%.,@  @#%,.@
+                       MySql/MariaDb
+'
+
+
+
+
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+sleep 0.2
+
+figlet -f standard "TOOL_by"
+
+sleep 0.2
+
+figlet -f standard "WLS"
+
+sleep 0.2
+
+echo "This Tool is Developed by https://github.com/WLS-SD under GNU GENERAL PUBLIC LICENSE Version 3.0."
+
+
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+
+
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+echo 'To Use PyServer Follow the steps :~'
+echo "Step 1 :~~ Enter 'exit' command to Exit Terminal & Relaunch Terminal & "
+echo "Step 2 :~~ To Start Python-Http-Server execute --->>> 'pyserver'"
